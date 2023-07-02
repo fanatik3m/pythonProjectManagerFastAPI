dacc883930ba898f6ee0e76d17d263bdf6a7be39
Станислав <stanislavskuratovich37@gmail.com>
2023-07-02 21:17:08 +0300

    Fixed some things

--- database.py
+++ database.py
@@ -1,7 +1,5 @@
 from typing import AsyncGenerator
 
-from fastapi import Depends
-from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
